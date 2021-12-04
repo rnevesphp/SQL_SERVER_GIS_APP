65b8f12a0aa4b347ab02c110a152fa5b2dcbc09c
Romeu <romeu.esp@gmail.com>
2021-12-04 22:08:46 +0100

    test

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var x = document.getElementById("demo");
 
 function getLocation() {
    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showPosition, showError);
+      navigator.geolocation.watchPosition(showPosition, showError);
    } else {
       x.innerHTML = "Geolocation is not supported by this browser.";
    }
